170046c286acc9e026c7f0fac6020fd6eec9a5cb
Matt Evangelisti <mjevange@gmail.com>
2020-05-28 20:35:18 -0400

    hotdog -> hotdogg
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I9f72303041768cc4a56d5c6b7bde1a441febda29

--- lineage_hotdogg.mk
+++ lineage_hotdogg.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hotdog device
-$(call inherit-product, device/oneplus/hotdog/device.mk)
+# Inherit from hotdogg device
+$(call inherit-product, device/oneplus/hotdogg/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hotdog
-PRODUCT_DEVICE := hotdog
+PRODUCT_NAME := lineage_hotdogg
+PRODUCT_DEVICE := hotdogg
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := OnePlus 7T Pro
+PRODUCT_MODEL := OnePlus 7T Pro 5G
 PRODUCT_MANUFACTURER := OnePlus
 
 PRODUCT_AAPT_CONFIG := xxxhdpi
@@ -30,10 +30,11 @@ TARGET_SCREEN_HEIGHT := 3120
 TARGET_SCREEN_WIDTH := 1440
 
 # Build info
-BUILD_FINGERPRINT := "OnePlus/OnePlus7TPro_EEA/OnePlus7TPro:10/QKQ1.190716.003/1910120055:user/release-keys"
+BUILD_FINGERPRINT := "OnePlus/OnePlus7TProNR/OnePlus7TProNR:10/QKQ1.190716.003/2001101147/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=OnePlus7TPro \
-    PRODUCT_NAME=OnePlus7TPro \
-    PRIVATE_BUILD_DESC="OnePlus7TPro_EEA-user 10 QKQ1.190716.003 1910120055 release-keys"
+    PRODUCT_DEVICE=OnePlus7TProNR \
+    PRODUCT_NAME=OnePlus7TProNR \
+    PRIVATE_BUILD_DESC="OnePlus7TProNR-user 10 QKQ1.190716.003 2001101147 release-keys"
+ 
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
