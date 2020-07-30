e0b17155296f82f5db447fa5f5a7fd14c010c3d4
Matt Filetto <matt.filetto@gmail.com>
2020-07-30 07:31:10 -0700

    blissify
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- bliss_hotdogg.mk
+++ bliss_hotdogg.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from hotdogg device
 $(call inherit-product, device/oneplus/hotdogg/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BlissRoms stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_hotdogg
