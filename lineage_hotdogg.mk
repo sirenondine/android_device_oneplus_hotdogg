3876bca0c89622c8514adadf6b329e2292ab9092
sirenondine <50174839+sirenondine@users.noreply.github.com>
2020-08-15 12:29:45 -0500

    zenx

--- lineage_hotdogg.mk
+++ lineage_hotdogg.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/hotdogg/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/zenx/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hotdogg
+PRODUCT_NAME := zenx_hotdogg
 PRODUCT_DEVICE := hotdogg
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := HD1925
